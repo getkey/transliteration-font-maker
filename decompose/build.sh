b99ce13db5354106dcf7590ec56714b1b0fa99bb
Julien Mourer <getkey@getkey.eu>
2025-03-02 12:57:52 +0100

    Split out tonos and diaresis generation

--- decompose/build.sh
+++ decompose/build.sh
@@ -3,4 +3,4 @@
 script_dir=$(dirname "$0")
 
 cp "$script_dir"/traditional_greek_romanization_base.tsv "$script_dir"/../transcriptions/traditional_greek_romanization.tsv
-"$script_dir"/decompose.py -b "$script_dir"/traditional_greek_romanization_base.tsv -c "$script_dir"/extended.tsv >> "$script_dir"/../transcriptions/traditional_greek_romanization.tsv
+"$script_dir"/decompose.py -b "$script_dir"/traditional_greek_romanization_base.tsv -c "$script_dir"/tonos_dialytika.tsv -c "$script_dir"/extended.tsv >> "$script_dir"/../transcriptions/traditional_greek_romanization.tsv
