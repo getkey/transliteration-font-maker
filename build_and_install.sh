f90871f5df84c20f8b392e933f2e927427ee8ce8
Julien Mourer <getkey@getkey.eu>
2025-03-03 20:04:48 +0100

    Properly name the font
    The font weight is a metadata of the file. It should not be part of the font name.

--- build_and_install.sh
+++ build_and_install.sh
@@ -2,7 +2,7 @@
 
 fontfile=../liberation-fonts/src/LiberationSerif-Regular.sfd
 swap_csv=./transcriptions/traditional_greek_romanization.tsv
-outname='Apeleutherosis Serif Regular'
+outname='Apeleutherosis Serif'
 
 fontforge -script swap.py -n "$outname" -t "$swap_csv" -o ~/.local/share/fonts/"$outname".otf "$fontfile"
 fc-cache -f
