a213c2142e56596b5fcd68225d0bea283fcd09ae
Julien Mourer <getkey@getkey.eu>
2025-03-17 21:29:04 +0100

    Fetch liberation from Github

--- shell.nix
+++ shell.nix
@@ -1,7 +1,20 @@
 { pkgs ? import <nixpkgs> {} }:
-	pkgs.mkShell {
-		buildInputs = [
-			pkgs.fontforge-gtk
-			pkgs.python3
-		];
+
+let
+	liberation-fonts = pkgs.fetchFromGitHub {
+		owner = "liberationfonts";
+		repo = "liberation-fonts";
+		rev = "49e1358e4017577429c9f8c39a3e6e879093264e";
+		sha256 = "sha256-GCCl2sGfYZ56tLNbvBvyzMZJoJ8kbHFtLpkwNRBKll4=";
+	};
+in
+pkgs.mkShell {
+	buildInputs = [
+		pkgs.fontforge-gtk
+		pkgs.python3
+	];
+
+	shellHook = ''
+		export LIBERATION_FONTS=${liberation-fonts}
+	'';
 }
